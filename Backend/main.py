5cd6e68a5a9032d7cd2ce0f178c58ffc4f8ab0a8
ANIS SAYYAD <156597008+Anissayyad47@users.noreply.github.com>
2025-07-18 23:25:49 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 from fastapi.responses import StreamingResponse
 from PIL import Image
 import io
-12
+123
 from fastapi import FastAPI, File, UploadFile, HTTPException
 import os
 import subprocess
